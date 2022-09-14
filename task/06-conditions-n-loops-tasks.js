7a985d0d103d39ab40b7053c61c68d5384566ebe
Valentin Turuk <valentin.turuk@gmail.com>
2022-09-14 15:58:50 +0300

    Item 5 in task 6 (resolved with js) is added

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -150,9 +150,19 @@ isTriangle(1, 2, 3)
  *  
  */
 function doRectanglesOverlap(rect1, rect2) {
-    throw new Error('Not implemented');
+    let matchHeight = false;
+    let matchWidth = false;
+
+    if ((rect1.top <= rect2.top && rect1.top + rect1.height >= rect2.top) || (rect1.top <= rect2.top + rect2.height && rect1.top + rect1.height >= rect2.top + rect2.height)) {
+        matchHeight = true;
+    } else if ((rect1.left <= rect2.left && rect1.left + rect1.width >= rect2.left) || (rect1.left <= rect2.left + rect2.width && rect1.left + rect1.width >= rect2.left + rect2.width)) {
+        matchWidth = true;
+    }
+
+    return matchHeight || matchWidth;
 }
 
+doRectanglesOverlap({ top: 0, left: 0, width: 10, height: 10 }, { top: 5, left: 5, width: 20, height: 20 });
 
 /**
  * 6) Returns true, if point lies inside the circle, otherwise false.
